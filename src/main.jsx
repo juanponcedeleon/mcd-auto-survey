1ebb17565ffeeb3c020c93688be9dce9c58c8dcd
Juan P. <jmaster10ultimate@gmail.com>
2025-08-15 01:33:02 -0700

    added landing page and added jwt to tmp-api

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 import './index.css'
-import App from './App.jsx'
+import Dashboard from './Dashboard.jsx'
+import Login from './Login.jsx'
+import Landing from './Landing.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <nav>
+        <NavLink to="/login" className="navlink primary-btn">Login</NavLink>
+      </nav> */}
+      <Routes>
+        <Route path="/" element={<Landing />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
